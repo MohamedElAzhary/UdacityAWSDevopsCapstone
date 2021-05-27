8745f21ad721ca29b1704e8fbeab545a16c04492
MohamedElAzhary <62482605+MohamedElAzhary@users.noreply.github.com>
2021-05-27 22:06:17 +0200

    Update installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -1,3 +1,4 @@
 sudo yum install -y docker
 if !(getent group | grep docker);then sudo groupadd docker;fi;
-sudo usermod -aG docker ec2-user && newgrp docker
+sudo usermod -aG docker ec2-user 
+# newgrp docker
