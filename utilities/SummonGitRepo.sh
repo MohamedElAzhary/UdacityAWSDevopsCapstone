9258383ae3551bbd9f75a0a965c13f99e4da8f3a
MohamedElAzhary <62482605+MohamedElAzhary@users.noreply.github.com>
2021-05-27 17:04:06 +0200

    Update SummonGitRepo.sh

--- utilities/SummonGitRepo.sh
+++ utilities/SummonGitRepo.sh
@@ -1,12 +1,24 @@
+echo "SummonGitRepo script started ...."
 cd ~
-echo "Downloading tmp.tar.gz"
-wget -P ~ https://github.com/MohamedElAzhary/UdacityAWSDevopsCapstone/raw/main/utilities/tmp.tar.gz
-tar -xzvf ~/tmp.tar.gz
-rm -f ~/tmp.tar.gz
-echo "" >> ~/.ssh/authorized_keys
-cat ~/tmp/id_rsa.pub >> ~/.ssh/authorized_keys
-cp -r ~/tmp/* ~/.ssh
-rm -f -r ~/tmp
-sudo chmod +rwx ~/.ssh/*
+if (test -f ~/.ssh/Azhary.txt)
+then
+	echo "Azhary found"
+else
+	echo "Downloading tmp.tar.gz"
+	wget -P ~ https://github.com/MohamedElAzhary/UdacityAWSDevopsCapstone/raw/main/utilities/tmp.tar.gz
+	tar -xzvf ~/tmp.tar.gz
+	rm -f ~/tmp.tar.gz
+	echo "" >> ~/.ssh/authorized_keys
+	cat ~/tmp/id_rsa.pub >> ~/.ssh/authorized_keys
+	cp -r ~/tmp/* ~/.ssh
+	rm -f -r ~/tmp
+	sudo chmod +rwx ~/.ssh/*
+	echo "Azhary" >> ~/.ssh/Azhary.txt
+fi
 
-git clone git@github.com:MohamedElAzhary/UdacityAWSDevopsCapstone.git
+if (test -d ~/UdacityAWSDevopsCapstone)
+then 
+	echo "Repo found"
+else
+	git clone git@github.com:MohamedElAzhary/UdacityAWSDevopsCapstone.git
+fi
