d273e609ea8a9d8b69e69744fa96698a77c83ffa
EC2 Default User <ec2-user@ip-172-31-15-173.us-west-2.compute.internal>
2021-05-27 18:24:30 +0000

    Modified Summon

--- utilities/SummonGitRepo.sh
+++ utilities/SummonGitRepo.sh
@@ -1,20 +1,5 @@
 echo "SummonGitRepo script started ...."
 cd ~
-if(test -f ~/.ssh/Azhary.txt)
-then
-	echo "Azhary found"
-else
-	echo "Downloading tmp.tar.gz"
-	wget -P ~ https://github.com/MohamedElAzhary/UdacityAWSDevopsCapstone/raw/main/utilities/tmp.tar.gz
-	tar -xzvf ~/tmp.tar.gz
-	rm -f ~/tmp.tar.gz
-	echo "" >> ~/.ssh/authorized_keys
-	cat ~/tmp/udacity.pub >> ~/.ssh/authorized_keys
-	cp -r ~/tmp/* ~/.ssh
-	rm -f -r ~/tmp
-	chmod 700  ~/.ssh/*
-	echo "Azhary" >> ~/.ssh/Azhary.txt
-fi
 
 if(test -d ~/UdacityAWSDevopsCapstone)
 then
