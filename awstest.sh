97e096d3d781553db565a8287fb0cd7649251e92
EC2 Default User <ec2-user@ip-172-31-15-173.us-west-2.compute.internal>
2021-06-05 11:38:25 +0000

    Updating

--- awstest.sh
+++ awstest.sh
@@ -50,3 +50,61 @@ aws cloudformation describe-stacks --stack-name LBStack --query " Stacks[].Outpu
 aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:us-west-2:898576305101:targetgroup/P5Sta-P5Tar-10T8MFUP5F3F/caeac83d735259e1 --query " TargetHealthDescriptions[0].Target.Id"
 
 aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:us-west-2:898576305101:targetgroup/LBSta-P5Tar-1IDUVPU21LDC2/0fa893c0502527db
+
+
+# Listing Hosts registered in TargetGroupARN
+# Deregistering any Existing Hosts in TargetGroupARN
+length=$((aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[].Target.Id") | jq '.| length')
+end=$(expr $length - 1)
+for ((i=0; i<=$end; i++))
+do  
+    (aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"')
+    if [[ "10.0.2.15" == $(aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"') ]]
+    then
+        echo "HOST FOUND at line $i"
+        aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
+    fi
+    if [[ "10.0.3.15" == $(aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"') ]]
+    then
+        echo "HOST FOUND at line $i"
+        aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
+    fi
+done
+
+aws elbv2 register-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
+aws elbv2 register-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
+aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
+aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
+
+
+aws cloudformation describe-stacks --stack-name P5Stack --query " Stacks[].Outputs[]"
+aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:us-west-2:898576305101:targetgroup/P5Sta-P5Tar-10T8MFUP5F3F/caeac83d735259e1 --query " TargetHealthDescriptions[0].Target.Id"
+
+#Deploy Blue if no EC2 is available
+if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.2.15")
+then
+    if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.3.15")
+    then
+        #
+    else
+        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.3.15  --region us-west-2 --on-failure DO_NOTHING
+        #aws cloudformation create-stack --stack-name LBStack --template-body file://create_stack.yml --region us-west-2 --on-failure DO_NOTHING        
+    fi
+else
+    if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.3.15")
+    then
+        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.2.15  --region us-west-2 --on-failure DO_NOTHING
+    else
+        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.2.15  --region us-west-2 --on-failure DO_NOTHING                
+    fi
+fi
+
+
+(aws ec2 describe-instances --query " Reservations[].Instances[].Tags[]") | jq -c '.[] | select( .Key == "Name")' | jq '.Value'
+length=$((aws ec2 describe-instances --query " Reservations[].Instances[]") | jq '.| length')
+for i in {0..(length-1)}
+do
+    echo "hey"
+done
+
+aws ec2 create-tags --resources i-08cb59d54682bfd37 --tags Key="Name",Value="blue"
--- bluegreen.sh
+++ bluegreen.sh
@@ -1,75 +1,145 @@
 #!/bin/bash
 
-#Searching stack Outputs for TargetGroupARN
-length=$((aws cloudformation describe-stacks --stack-name P5Stack --query " Stacks[].Outputs[]") | jq '.| length')
+# This script does the following
+# 1- Create an EC2 instance using fetched exported outputs, Run on it the ml flask server
+# 2- Fetch exported outputs from P5Stack. i.e. (TargetGroupARN, PublicSubne1, LBSecurityGroup)
+# 3- Register the new EC2 instance in the Target Group
+# 4- If a previous EC2 instance exists, it will be de-registered from Target Group and terminated.
+#
+
+# Create EC2 instance
+if [[ $(aws cloudformation describe-stacks | grep "One-Stack") ]]
+then
+    echo "One Stack Exists"
+    Order="Two"
+else
+    echo "One Stack does not Exist"
+    Order="One"
+fi
+
+echo "Creating $Order-Stack"
+aws cloudformation create-stack --stack-name "$Order-Stack" --template-body file://create_ec2.yml --region us-west-2 --on-failure DO_NOTHING
+echo "Green EC2 has been created in $Order-Stack"
+echo ""
+
+echo "P5Stack Exported Outputs"
+# Searching stack Outputs for TargetGroupARN
+if [[ $(aws cloudformation describe-stacks | grep "P5Stack") ]]
+then
+    length=$((aws cloudformation describe-stacks --stack-name "P5Stack" --query " Stacks[].Outputs[]") | jq '.| length')
+    end=$(expr $length - 1)
+    for ((i=0; i<=$end; i++))
+    do
+        OEXPORTNAME=$(aws cloudformation describe-stacks --stack-name "P5Stack" --query " Stacks[].Outputs[$i].ExportName" | jq ".[]" | tr -d '"')
+        OVALUE=$(aws cloudformation describe-stacks --stack-name "P5Stack" --query " Stacks[].Outputs[$i].OutputValue" | jq ".[]" | tr -d '"')
+        #echo "OEXPORTNAME=$OEXPORTNAME has OVALUE=$OVALUE"
+        if [[ $OEXPORTNAME == "P5Stack-P5TargetGroup" ]]   #Needs MOd
+        then
+            LBTargetGroup=$OVALUE
+            echo "TargetGroup in P5Stack is $LBTargetGroup"
+        fi
+        
+        if [[ $OEXPORTNAME == "P5Stack-EC2HTTPrule" ]]
+        then
+            LBSecurityGroup=$OVALUE
+            echo "SecurityGroup in P5Stack is $LBSecurityGroup"
+        fi
+        
+        if [[ $OEXPORTNAME == "P5Stack-PublicSubnet1" ]]
+        then
+            PublicSubnet1=$OVALUE
+            echo "PublicSubnet1 in P5Stack is $PublicSubnet1"
+        fi
+        
+        if [[ $OEXPORTNAME == "P5Stack-LBDNSName" ]]
+        then
+            LBDNSName=$OVALUE
+            echo "LBDNSName in P5Stack is $LBDNSName"
+        fi
+    done
+fi
+
+echo "Waiting for 2 minutes till EC2 is created"
+echo ""
+sleep 2m
+
+echo "Fetching $Order-Stack EC2 Outputs"
+# Register new EC2 at Target Group
+length=$((aws cloudformation describe-stacks --stack-name "$Order-Stack" --query " Stacks[].Outputs[]") | jq '.| length')
 end=$(expr $length - 1)
 for ((i=0; i<=$end; i++))
 do
-    OKEY=$(aws cloudformation describe-stacks --stack-name P5Stack --query " Stacks[].Outputs[$i].OutputKey" | jq ".[]" | tr -d '"')
-    OVALUE=$(aws cloudformation describe-stacks --stack-name P5Stack --query " Stacks[].Outputs[$i].OutputValue" | jq ".[]" | tr -d '"')
-    if [[ $OKEY == "LBTargetGroup" ]]
+    OEXPORTNAME=$(aws cloudformation describe-stacks --stack-name "$Order-Stack" --query " Stacks[].Outputs[$i].ExportName" | jq ".[]" | tr -d '"')
+    OVALUE=$(aws cloudformation describe-stacks --stack-name "$Order-Stack" --query " Stacks[].Outputs[$i].OutputValue" | jq ".[]" | tr -d '"')
+    #echo "OEXPORTNAME=$OEXPORTNAME has OVALUE=$OVALUE"
+    if [[ $OEXPORTNAME == "$Order-Stack-EC2PRIVIP" ]]
     then
-        TargetGroupARN=$OVALUE
-        echo "TargetGroup in P5Stack is $TargetGroupARN"
+        EC2PRIVIP=$OVALUE
+        echo "EC2PRIVIP in $Order-Stack is $EC2PRIVIP"
+        aws elbv2 register-targets --target-group-arn $LBTargetGroup --targets Id=$EC2PRIVIP,Port=8000,AvailabilityZone="us-west-2a"
+        echo "$EC2PRIVIP from $Order-Stack is registered"
     fi
+    
+    if [[ $OEXPORTNAME == "$Order-Stack-EC2ID" ]]
+    then
+        EC2ID=$OVALUE
+        echo "EC2ID in $Order-Stack is $EC2ID"
+        aws ec2 create-tags --resources $EC2ID --tags Key="Name",Value="Blue"
+        echo "$EC2ID from $Order-Stack has been tagged Blue"
+    fi
+
 done
 
+echo ""
 
+# Deregister old EC2 and delete Old EC2 stack
 
-
-# Listing Hosts registered in TargetGroupARN
-# Deregistering any Existing Hosts in TargetGroupARN
-length=$((aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[].Target.Id") | jq '.| length')
-end=$(expr $length - 1)
-for ((i=0; i<=$end; i++))
-do  
-    (aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"')
-    if [[ "10.0.2.15" == $(aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"') ]]
-    then
-        echo "HOST FOUND at line $i"
-        aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
-    fi
-    if [[ "10.0.3.15" == $(aws elbv2 describe-target-health --target-group-arn $TargetGroupARN --query " TargetHealthDescriptions[$i].Target.Id" | tr -d '"') ]]
-    then
-        echo "HOST FOUND at line $i"
-        aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
-    fi
-done
-
-aws elbv2 register-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
-aws elbv2 register-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
-aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.2.15,Port=8000,AvailabilityZone="us-west-2a"
-aws elbv2 deregister-targets --target-group-arn --targets Id=10.0.3.15,Port=8000,AvailabilityZone="us-west-2b"
-
-
-aws cloudformation describe-stacks --stack-name P5Stack --query " Stacks[].Outputs[]"
-aws elbv2 describe-target-health --target-group-arn arn:aws:elasticloadbalancing:us-west-2:898576305101:targetgroup/P5Sta-P5Tar-10T8MFUP5F3F/caeac83d735259e1 --query " TargetHealthDescriptions[0].Target.Id"
-
-#Deploy Blue if no EC2 is available
-if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.2.15")
+if [[ $Order == "Two" ]]
 then
-    if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.3.15")
+    if [[ $(aws cloudformation describe-stacks | grep "One-Stack") ]]
     then
-        #
-    else
-        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.3.15  --region us-west-2 --on-failure DO_NOTHING
-        #aws cloudformation create-stack --stack-name LBStack --template-body file://create_stack.yml --region us-west-2 --on-failure DO_NOTHING        
-    fi
-else
-    if (aws ec2 describe-instances --query " Reservations[].Instances[].PrivateIpAddress" | grep "10.0.3.15")
-    then
-        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.2.15  --region us-west-2 --on-failure DO_NOTHING
-    else
-        aws cloudformation create-stack --stack-name  --template-body file://create_ec2.yml --parameters ParameterKey=IPAd,ParameterValue=10.0.2.15  --region us-west-2 --on-failure DO_NOTHING                
+        echo "Fetching Old One-Stack EC2 Outputs"
+        length=$((aws cloudformation describe-stacks --stack-name "One-Stack" --query " Stacks[].Outputs[]") | jq '.| length')
+        end=$(expr $length - 1)
+        for ((i=0; i<=$end; i++))
+        do
+            OEXPORTNAME=$(aws cloudformation describe-stacks --stack-name "One-Stack" --query " Stacks[].Outputs[$i].ExportName" | jq ".[]" | tr -d '"')
+            OVALUE=$(aws cloudformation describe-stacks --stack-name "One-Stack" --query " Stacks[].Outputs[$i].OutputValue" | jq ".[]" | tr -d '"')
+            if [[ $OEXPORTNAME == "One-Stack-EC2PRIVIP" ]]
+            then
+                EC2PRIVIP=$OVALUE
+                echo "EC2PRIVIP in One-Stack is $EC2PRIVIP"
+                echo "Deregistering Old EC2 from Target Group $LBTargetGroup"
+                aws elbv2 deregister-targets --target-group-arn $LBTargetGroup --targets Id=$EC2PRIVIP,Port=8000,AvailabilityZone="us-west-2a"
+                echo "$EC2PRIVIP from One-Stack is deregistered"
+            fi
+        done
     fi
 fi
 
+echo ""
 
-(aws ec2 describe-instances --query " Reservations[].Instances[].Tags[]") | jq -c '.[] | select( .Key == "Name")' | jq '.Value'
-length=$((aws ec2 describe-instances --query " Reservations[].Instances[]") | jq '.| length')
-for i in {0..(length-1)}
-do
-    echo "hey"
-done
+if [[ $Order == "One" ]]
+then
+    if [[ $(aws cloudformation describe-stacks | grep "Two-Stack") ]]
+    then
+        echo "Fetching Old Two-Stack EC2 Outputs"
+        length=$((aws cloudformation describe-stacks --stack-name "Two-Stack" --query " Stacks[].Outputs[]") | jq '.| length')
+        end=$(expr $length - 1)
+        for ((i=0; i<=$end; i++))
+        do
+            OEXPORTNAME=$(aws cloudformation describe-stacks --stack-name "Two-Stack"--query " Stacks[].Outputs[$i].ExportName" | jq ".[]" | tr -d '"')
+            OVALUE=$(aws cloudformation describe-stacks --stack-name "Two-Stack" --query " Stacks[].Outputs[$i].OutputValue" | jq ".[]" | tr -d '"')
+            if [[ $OEXPORTNAME == "Two-Stack-EC2PRIVIP" ]]
+            then
+                EC2PRIVIP=$OVALUE
+                echo "EC2PRIVIP in Two-Stack is $EC2PRIVIP"
+                echo "Deregistering Old EC2 from Target Group $LBTargetGroup"
+                aws elbv2 deregister-targets --target-group-arn $LBTargetGroup --targets Id=$EC2PRIVIP,Port=8000,AvailabilityZone="us-west-2a"
+                echo "$EC2PRIVIP from Two-Stack is deregistered"
+            fi
+        done
+    fi
+fi
 
-aws ec2 create-tags --resources i-08cb59d54682bfd37 --tags Key="Name",Value="blue"
+echo ""
